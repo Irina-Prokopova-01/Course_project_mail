f2af873cc7f6d66b4d43b36edd9dd87481f40102
Irina Prokopova <irina-prokopova.style@yandex.ru>
2024-11-06 12:41:57 +0300

    add forms.py, init

--- mail/urls.py
+++ mail/urls.py
@@ -1,11 +1,27 @@
 from django.urls import path
 
 from mail.apps import MailConfig
-from mail.views import MailingView, RecipientListView
+from mail.views import MailingView, RecipientDetailView, RecipientCreateView, RecipientDeleteView, RecipientUpdateView, RecipientListView, MessageListView, MessageCreateView, MessageDeleteView, MailingDetailView, MessageUpdateView, MailingCreateView, MailingDeleteView, MailingUpdateView, MailingListView, MessageDetailView, AttemptsListView
 
 app_name = MailConfig.name
 
 urlpatterns = [
     path("", MailingView.as_view(), name="main"),
     path("recipients/", RecipientListView.as_view(), name="recipient_list"),
+    path("recipients/<int:pk>/", RecipientDetailView.as_view(), name="recipient_detail"),
+    path("recipients/create/", RecipientCreateView.as_view(), name="recipient_create"),
+    path("recipients/<int:pk>/update/", RecipientUpdateView.as_view(), name="recipient_update"),
+    path("recipients/<int:pk>/delete/", RecipientDeleteView.as_view(), name="recipient_delete"),
+    path("messages/", MessageListView.as_view(), name="message_list"),
+    path("messages/<int:pk>/", MessageDetailView.as_view(), name="message_detail"),
+    path("messages/create/", MessageCreateView.as_view(), name="message_create"),
+    path("messages/<int:pk>/update/", MessageUpdateView.as_view(), name="message_update"),
+    path("messages/<int:pk>/delete/", MessageDeleteView.as_view(), name="message_delete"),
+    path("mailing/", MailingListView.as_view(), name="mailing_list"),
+    path("mailing/<int:pk>/", MailingDetailView.as_view(), name="mailing_detail"),
+    path("mailing/create/", MailingCreateView.as_view(), name="mailing_create"),
+    path("mailing/<int:pk>/update/", MailingUpdateView.as_view(), name="mailing_update"),
+    path("mailing/<int:pk>/delete/", MailingDeleteView.as_view(), name="mailing_delete"),
+    path("mailing_attempts/", AttemptsListView.as_view(), name="mailing_attempts_list"),
+
 ]
--- mail/views.py
+++ mail/views.py
@@ -1,12 +1,10 @@
-from django.conf import settings
-from django.core.mail import send_mail
 
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import DetailView, ListView, TemplateView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from .models import Mailing, Attempts, Message, Recipient
+from .forms import RecipientForm, MailingForm, MessageForm
 
 
 class MailingView(TemplateView):
@@ -17,3 +15,104 @@ class MailingView(TemplateView):
 class RecipientListView(ListView):
     model = Recipient
     template_name = "mail/recipient_list.html"
+
+
+class RecipientDetailView(DetailView):
+    model = Recipient
+    template_name = "mail/recipient_detail.html"
+
+
+class RecipientCreateView(CreateView):
+    model = Recipient
+    form_class = RecipientForm
+    template_name = "mail/recipient_form.html"
+    success_url = reverse_lazy("mail:recipient_list")
+
+
+class RecipientUpdateView(UpdateView):
+    model = Recipient
+    form_class = RecipientForm
+    template_name = "mail/recipient_form.html"
+    success_url = reverse_lazy("mail:recipient_list")
+
+    def get_success_url(self):
+        return reverse_lazy("mail:recipient_detail", kwargs={"pk": self.object.pk})
+
+
+class RecipientDeleteView(DeleteView):
+    model = Recipient
+    template_name = "mail/recipient_confirm_delete.html"
+    success_url = reverse_lazy("mail:recipient_list")
+
+
+class MessageListView(ListView):
+    model = Message
+    template_name = "mail/message_list.html"
+
+
+class MessageDetailView(DetailView):
+    model = Message
+    template_name = "mail/message_detail.html"
+
+
+class MessageCreateView(CreateView):
+    model = Message
+    form_class = MessageForm
+    template_name = "mail/message_form.html"
+    success_url = reverse_lazy("mail:message_list")
+
+
+class MessageUpdateView(UpdateView):
+    model = Message
+    form_class = MessageForm
+    template_name = "mail/message_form.html"
+    success_url = reverse_lazy("mail:message_list")
+
+    def get_success_url(self):
+        return reverse_lazy("mail:message_detail", kwargs={"pk": self.object.pk})
+
+
+class MessageDeleteView(DeleteView):
+    model = Message
+    template_name = "mail/message_confirm_delete.html"
+    success_url = reverse_lazy("mail:message_list")
+
+
+class MailingListView(ListView):
+    model = Mailing
+    template_name = "mail/mailing_list.html"
+
+
+
+class MailingDetailView(DetailView):
+    model = Mailing
+    template_name = "mail/mailing_detail.html"
+
+
+class MailingCreateView(CreateView):
+    model = Mailing
+    form_class = MailingForm
+    template_name = "mail/mailing_form.html"
+    success_url = reverse_lazy("mail:mailing_list")
+
+
+class MailingUpdateView(UpdateView):
+    model = Mailing
+    form_class = MailingForm
+    template_name = "mail/mailing_form.html"
+    success_url = reverse_lazy("mail:mailing_list")
+
+    def get_success_url(self):
+        return reverse_lazy("mail:mailing_detail", kwargs={"pk": self.object.pk})
+
+
+class MailingDeleteView(DeleteView):
+    model = Mailing
+    template_name = "mail/mailing_confirm_delete.html"
+    success_url = reverse_lazy("mail:mailing_list")
+
+
+class AttemptsListView(ListView):
+    model = Attempts
+    template_name = "mail/mailing_attempts_list.html"
+
